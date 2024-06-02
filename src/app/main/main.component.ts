ff4c6b2dce645c4112575654be140bb92f510546
OrlolvareZ <orlolvarez@gmail.com>
2024-06-02 00:01:35 -0600

    feat: 2024-06-01 implemented placeholders

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,74 +1,98 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { MatButton, MatFabButton, MatIconButton } from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatCard } from '@angular/material/card';
-import { MatGridList, MatGridListModule, MatGridTile } from '@angular/material/grid-list';
-import { MatIcon } from '@angular/material/icon';
-import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressBar } from '@angular/material/progress-bar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { environment } from '../environment';
+import { DialogComponent } from '../dialog/dialog.component';
 
 @Component({
-  selector: 'app-main',
-  standalone: true,
-  imports: [
-    MatButton,
-    MatIconButton,
-    MatFabButton,
-    MatSnackBarModule,
-    MatCard,
-    MatGridListModule,
-    MatGridList,
-    MatGridTile,
-    MatIcon,
-    MatProgressSpinner, 
-    HttpClientModule,
-    AngularFireModule,
-    AngularFireDatabaseModule,
-    AngularFirestoreModule,
-    AngularFireStorageModule,
-  ],
-  templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+    selector: 'app-main',
+    standalone: true,
+    imports: [
+        // Material
+        MatButtonModule,
+        MatSnackBarModule,
+        MatCard,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatToolbarModule,
+        MatMenuModule,
+        MatDialogModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatDividerModule,
+        MatProgressBar,
+        // Angular
+        HttpClientModule,
+        FormsModule,
+        // Firebase
+        AngularFireModule,
+        AngularFirestoreModule,
+    ],
+    templateUrl: './main.component.html',
+    styleUrl: './main.component.scss'
 })
 export class MainComponent {
 
-    accessToken = "6cb0a953824a29cbe8d06deebb8b18e362149c9a";
-    deviceID = "2f002e001847393035313137";
-    particleUrlBase = `https://api.particle.io/v1/devices/${this.deviceID}`;
-    particleUrls = {
-        // Syncs shots left in the inhaler and mails to send to
-        sync: `${this.particleUrlBase}/sync`,
-    }
+    // Collections
+    contacts: string[] | null = null;
+
+    // App state
+    isSyncing = true;
+
+    isLoadingContacts = true;
+    contactsChanged = false;
+
+    dialogRef: MatDialogRef<any> | null = null;
+
+    // UI
+    dialogSize = {
+        width: '80%',
+        height: 'auto',
+    };
 
     constructor(
         private http: HttpClient,
         private snack: MatSnackBar,
         private db: AngularFirestore,
-    ) {}
+        private dialog: MatDialog,
+    ) { }
 
-    /**
-     * Request example:
-     * 
-     * this.http.post(
-            `${this.url}?access_token=${this.accessToken}`,
-            'params=placeholder',
+    openEmailDialog(emailView: any) {
+
+        this.dialogRef = this.dialog.open(DialogComponent, {
+            data: emailView,
+            ...this.dialogSize,
+        });
+
+        this.dialogRef.afterClosed().subscribe(result => {
+            this.dialogRef = null;
+        });
+
+    }
+
+    requestParticle(endpoint: string, params: string = '') {
+        return this.http.post(
+            `${environment.particle.particleUrlBase}${endpoint}?access_token=${environment.particle.accessToken}`,
+            params,
             {
                 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
             }
-        ).subscribe({
-            next: (response) => {
-                
-            },
-            error: (error) => {
-                this.snack.open('¡No se pudo alimentar a tu pez! 😢', 'Ok');
-                this.isFeeding = false;
-            }
-        });
-     */
+        );
+    }
 
 }
